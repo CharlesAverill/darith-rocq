b622cb18905112fd9ca3e7d37edc7087bfa34d04
Charles Averill <charlesaverill20@gmail.com>
2026-08-25 19:25:18 -0500

    Add multivariate

--- examples/QcAD.v
+++ examples/QcAD.v
@@ -7,24 +7,22 @@ Module QcAD := GDualAD QcField.
 Import QcAD Dual.
 
 Add Ring Qc_base_ring : QcField.rt.
-Add Field S_qc_field : QcField.ft.
+Add Field Qc_base_field : QcField.ft.
 
 Open Scope Qc_scope.
 
-(* Test cases *)
+(* Seed 1 at variable i, 0 elsewhere; dual environment at point a *)
+Definition seed (i : nat) : nat -> S := fun j => if Nat.eqb j i then 1%S else 0%S.
+Definition env_at (a : nat -> S) (i : nat) : nat -> D := fun j => (a j, seed i j).
 
-(* f(x) = x^2 *)
-Definition sq : Expr := Mul Var Var.
+(* Single-variable point: variable 0 at a, seed 1 *)
+Definition pt (a : S) : nat -> D := fun _ => (a, 1%S).
 
-(* f(x) = x^2 + 3x + 2 *)
-Definition poly : Expr :=
-    Add (Add (Mul Var Var) (Mul (Const (Q2Qc 3)) Var)) (Const (Q2Qc 2)).
+Notation X := (Var 0).
+Notation Y := (Var 1).
 
-(* f(x) = 1 / x *)
-Definition recip : Expr := Div (Const 1) Var.
-
-(* f(x) = x / (x + 1) *)
-Definition frac : Expr := Div Var (Add Var (Const 1)).
+Lemma Q2Qc_1 : Q2Qc 1 = 1%S.
+Proof. reflexivity. Qed.
 
 Lemma Q2Qc_2 : Q2Qc 2 = (1 + 1)%S.
 Proof. reflexivity. Qed.
@@ -32,90 +30,147 @@ Proof. reflexivity. Qed.
 Lemma Q2Qc_3 : Q2Qc 3 = (1 + 1 + 1)%S.
 Proof. reflexivity. Qed.
 
+(* Test cases *)
+
+(* f(x) = x^2 *)
+Definition sq : Expr := Mul X X.
+
+(* f(x) = x^2 + 3x + 2 *)
+Definition poly : Expr :=
+    Add (Add (Mul X X) (Mul (Const (Q2Qc 3)) X)) (Const (Q2Qc 2)).
+
+(* f(x) = 1 / x *)
+Definition recip : Expr := Div (Const 1) X.
+
+(* f(x) = x / (x + 1) *)
+Definition frac : Expr := Div X (Add X (Const 1)).
+
 (* Symbolic derivatives *)
 
-(* d/dx x^2 = 2x. *)
+(* d/dx x^2 = 2x *)
 Example deriv_sq :
-    forall a, du (eval_dual sq ((a, 1%S : S) : D)) = (Q2Qc 2 * a)%S.
+    forall a, du (eval_dual sq (pt a)) = (Q2Qc 2 * a)%S.
 Proof.
     intro a.
-    assert (Hdef : defined_at sq a) by (cbn; tauto).
-    destruct (eval_dual_correct sq a Hdef) as [_ Hd].
-    change S with Qc in *. rewrite Hd, Q2Qc_2. cbn.
+    assert (Hdef : defined_at sq (fun _ => a)) by (cbn; tauto).
+    destruct (eval_dual_correct sq (fun _ => a) (fun _ => 1%S) Hdef) as [_ Hd].
+    unfold pt. change S with Qc in *. rewrite Hd, Q2Qc_2. cbn.
     unfold one in *. ring.
 Qed.
 
-Add Field Qc_field : Qcft.
-
-(* d/dx (x^2 + 3x + 2) = 2x + 3. *)
+(* d/dx (x^2 + 3x + 2) = 2x + 3 *)
 Example deriv_poly :
-    forall a, du (eval_dual poly (a, 1%S : S)) = (Q2Qc 2 * a + Q2Qc 3)%S.
+    forall a, du (eval_dual poly (pt a)) = (Q2Qc 2 * a + Q2Qc 3)%S.
 Proof.
     intro a.
-    assert (Hdef : defined_at poly a) by (cbn; tauto).
-    destruct (eval_dual_correct poly a Hdef) as [_ Hd].
-    rewrite Hd. cbn.
-    rewrite Q2Qc_2, Q2Qc_3.
-    change S with Qc in *.
-    change QcField.add with Qcplus. change QcField.mul with Qcmult.
-    unfold zero, one. rewrite Qcmult_0_l, Qcplus_0_l, Qcplus_0_r.
-    ring.
+    assert (Hdef : defined_at poly (fun _ => a)) by (cbn; tauto).
+    destruct (eval_dual_correct poly (fun _ => a) (fun _ => 1%S) Hdef) as [_ Hd].
+    unfold pt.
+    change QcField.add with Qcplus.
+    change S with Qc in *. rewrite Hd. cbn. rewrite Q2Qc_2, Q2Qc_3.
+    change Qcplus with QcField.add.
+    change Qcmult with QcField.mul.
+    rewrite Q2Qc_1. ring.
 Qed.
 
-(* d/dx (1/x) = -1/x^2, where x <> 0. *)
+(* d/dx (1/x) = -1/x^2, where x <> 0 *)
 Example deriv_recip :
     forall a, (a <> 0)%S ->
-        du (eval_dual recip (a, 1%S)) = (- Qcinv (a * a))%S.
+        du (eval_dual recip (pt a)) = (- Qcinv (a * a))%S.
 Proof.
+    
     intros a Ha.
-    assert (Hdef : defined_at recip a) by (cbn; tauto).
-    destruct (eval_dual_correct recip a Hdef) as [_ Hd].
+    assert (Hdef : defined_at recip (fun _ => a)) by (cbn; tauto).
+    destruct (eval_dual_correct recip (fun _ => a) (fun _ => 1%S) Hdef) as [_ Hd].
+    unfold pt.
     change S with Qc in *. rewrite Hd. cbn.
     unfold one, zero in *.
     change QcField.add with Qcplus. change QcField.mul with Qcmult.
     change QcField.sub with Qcminus. change QcField.opp with Qcopp.
     unfold div.
-    now field.
+    Add Field Qc_raw_field : Qcft. now field.
 Qed.
 
-(* d/dx (x/(x+1)) = 1/(x+1)^2, where x + 1 <> 0. *)
+(* d/dx (x/(x+1)) = 1/(x+1)^2, where x + 1 <> 0 *)
 Example deriv_frac :
     forall a, ((a + 1) <> 0)%S ->
-        du (eval_dual frac (a, 1%S)) = Qcinv ((a + 1) * (a + 1)).
+        du (eval_dual frac (pt a)) = Qcinv ((a + 1) * (a + 1)).
 Proof.
     intros a Ha.
-    assert (Hdef : defined_at frac a) by (cbn; tauto).
-    destruct (eval_dual_correct frac a Hdef) as [_ Hd].
-    change S with Qc in *. rewrite Hd. cbn.
+    assert (Hdef : defined_at frac (fun _ => a)) by (cbn; tauto).
+    destruct (eval_dual_correct frac (fun _ => a) (fun _ => 1%S) Hdef) as [_ Hd].
+    unfold pt. change S with Qc in *. rewrite Hd. cbn.
     unfold one, zero in *.
     change QcField.add with Qcplus. change QcField.mul with Qcmult.
     change QcField.sub with Qcminus. change QcField.opp with Qcopp.
     unfold div. now field.
 Qed.
 
-(* f(x) = x^2 + 3x + 2 at x = 5:  value 42, derivative 13. *)
+(* f(x) = x^2 + 3x + 2 at x = 5:  value 42, derivative 13 *)
 Example poly_at_5_value :
-    real (eval_dual poly (Q2Qc 5, 1%S)) = Q2Qc 42.
+    real (eval_dual poly (pt (Q2Qc 5))) = Q2Qc 42.
 Proof. reflexivity. Qed.
 
 Example poly_at_5_deriv :
-    du (eval_dual poly (Q2Qc 5, 1%S)) = Q2Qc 13.
+    du (eval_dual poly (pt (Q2Qc 5))) = Q2Qc 13.
 Proof. reflexivity. Qed.
 
-(* f(x) = 1/x at x = 2:  value 1/2, derivative -1/4. *)
+(* f(x) = 1/x at x = 2:  value 1/2, derivative -1/4 *)
 Example recip_at_2_value :
-    real (eval_dual recip (Q2Qc 2, 1%S)) = Q2Qc (1 # 2).
+    real (eval_dual recip (pt (Q2Qc 2))) = Q2Qc (1 # 2).
 Proof. reflexivity. Qed.
 
 Example recip_at_2_deriv :
-    du (eval_dual recip (Q2Qc 2, 1%S)) = Q2Qc (- (1 # 4)).
+    du (eval_dual recip (pt (Q2Qc 2))) = Q2Qc (- (1 # 4)).
 Proof. reflexivity. Qed.
 
-(* f(x) = x/(x+1) at x = 3:  value 3/4, derivative 1/16. *)
+(* f(x) = x/(x+1) at x = 3:  value 3/4, derivative 1/16 *)
 Example frac_at_3_value :
-    real (eval_dual frac (Q2Qc 3, 1%S)) = Q2Qc (3 # 4).
+    real (eval_dual frac (pt (Q2Qc 3))) = Q2Qc (3 # 4).
 Proof. reflexivity. Qed.
 
 Example frac_at_3_deriv :
-    du (eval_dual frac (Q2Qc 3, 1%S)) = Q2Qc (1 # 16).
+    du (eval_dual frac (pt (Q2Qc 3))) = Q2Qc (1 # 16).
 Proof. apply Qceq_alt. reflexivity. Qed.
+
+(* Multivariate *)
+
+(* g(x, y) = x * y *)
+Definition prod_xy : Expr := Mul X Y.
+
+(* h(x, y) = x^2 * y + y *)
+Definition hxy : Expr := Add (Mul (Mul X X) Y) Y.
+
+(* Point (x, y) = (3, 4) *)
+Definition pt2 : nat -> S :=
+    fun j => match j with 0%nat => Q2Qc 3 | 1%nat => Q2Qc 4 | _ => 0%S end.
+
+(* g(3,4) = 12 *)
+Example prod_value :
+    real (eval_dual prod_xy (env_at pt2 0)) = Q2Qc 12.
+Proof. reflexivity. Qed.
+
+(* d/dx (x*y) = y = 4 *)
+Example prod_dx :
+    du (eval_dual prod_xy (env_at pt2 0)) = Q2Qc 4.
+Proof. reflexivity. Qed.
+
+(* d/dy (x*y) = x = 3 *)
+Example prod_dy :
+    du (eval_dual prod_xy (env_at pt2 1)) = Q2Qc 3.
+Proof. reflexivity. Qed.
+
+(* h(3,4) = 40 *)
+Example hxy_value :
+    real (eval_dual hxy (env_at pt2 0)) = Q2Qc 40.
+Proof. reflexivity. Qed.
+
+(* d/dx (x^2 y + y) = 2xy = 24 *)
+Example hxy_dx :
+    du (eval_dual hxy (env_at pt2 0)) = Q2Qc 24.
+Proof. reflexivity. Qed.
+
+(* d/dy (x^2 y + y) = x^2 + 1 = 10 *)
+Example hxy_dy :
+    du (eval_dual hxy (env_at pt2 1)) = Q2Qc 10.
+Proof. reflexivity. Qed.
--- theories/AD.v
+++ theories/AD.v
@@ -36,7 +36,7 @@ Proof. intros. unfold Ddiv. rewrite real_mul, real_Dinv. reflexivity. Qed.
 (* Single-variable expression language *)
 Inductive Expr : Set :=
 | Const (s : S)
-| Var
+| Var   (i : nat)
 | Add   (e1 e2 : Expr)
 | Sub   (e1 e2 : Expr)
 | Mul   (e1 e2 : Expr)
@@ -44,10 +44,10 @@ Inductive Expr : Set :=
 | Div   (e1 e2 : Expr).
 
 (* Evaluation over the base field *)
-Fixpoint eval (e : Expr) (x : S) : S :=
+Fixpoint eval (e : Expr) (x : nat -> S) : S :=
     match e with
     | Const c   => c
-    | Var       => x
+    | Var i     => x i
     | Add e1 e2 => (eval e1 x + eval e2 x)%S
     | Sub e1 e2 => (eval e1 x - eval e2 x)%S
     | Mul e1 e2 => (eval e1 x * eval e2 x)%S
@@ -56,10 +56,10 @@ Fixpoint eval (e : Expr) (x : S) : S :=
     end.
 
 (* Evaluation over the duals *)
-Fixpoint eval_dual (e : Expr) (x : D) : D :=
+Fixpoint eval_dual (e : Expr) (x : nat -> D) : D :=
     match e with
     | Const c   => inj c
-    | Var       => x
+    | Var i     => x i
     | Add e1 e2 => Dual.add (eval_dual e1 x) (eval_dual e2 x)
     | Sub e1 e2 => Dual.sub (eval_dual e1 x) (eval_dual e2 x)
     | Mul e1 e2 => Dual.mul (eval_dual e1 x) (eval_dual e2 x)
@@ -97,25 +97,25 @@ Fixpoint eval_dual (e : Expr) (x : D) : D :=
     derivative (Div e1 e2)
         (Div (Sub (Mul de1 e2) (Mul e1 de2)) (Mul e2 e2)). *)
 
-Fixpoint deriv (e : Expr) : Expr :=
+Fixpoint deriv (e : Expr) (v : nat -> S) : Expr :=
     match e with
     | Const _   => Const 0%S
-    | Var       => Const 1%S
-    | Add e1 e2 => Add (deriv e1) (deriv e2)
-    | Sub e1 e2 => Sub (deriv e1) (deriv e2)
-    | Mul e1 e2 => Add (Mul (deriv e1) e2) (Mul e1 (deriv e2))
-    | Opp e1    => Opp (deriv e1)
+    | Var j     => Const (v j)
+    | Add e1 e2 => Add (deriv e1 v) (deriv e2 v)
+    | Sub e1 e2 => Sub (deriv e1 v) (deriv e2 v)
+    | Mul e1 e2 => Add (Mul (deriv e1 v) e2) (Mul e1 (deriv e2 v))
+    | Opp e1    => Opp (deriv e1 v)
     | Div e1 e2 =>
         (* (e1' e2 - e1 e2') / e2^2 *)
-        Div (Sub (Mul (deriv e1) e2) (Mul e1 (deriv e2)))
+        Div (Sub (Mul (deriv e1 v) e2) (Mul e1 (deriv e2 v)))
             (Mul e2 e2)
     end.
 
 (* Well-definedness: Every denominator is nonzero *)
-Fixpoint defined_at (e : Expr) (x : S) : Prop :=
+Fixpoint defined_at (e : Expr) (x : nat -> S) : Prop :=
     match e with
     | Const _   => True
-    | Var       => True
+    | Var _     => True
     | Add e1 e2 => defined_at e1 x /\ defined_at e2 x
     | Sub e1 e2 => defined_at e1 x /\ defined_at e2 x
     | Mul e1 e2 => defined_at e1 x /\ defined_at e2 x
@@ -125,44 +125,46 @@ Fixpoint defined_at (e : Expr) (x : S) : Prop :=
 
 (* Correctness
 
-   After evaluating [e] over the duals at [a + eps], its real part
-   is [eval e a], and its dual part is [eval (deriv e) a]. *)
+   Evaluating an expression [e] over multivariate dual numbers gives two results:
+   1. The real part is the standard evaluation of the expression
+   2. The dual part is the partial derivative with respect to variable [i]. *)
 Theorem eval_dual_correct :
-    forall e a,
+    forall (e : Expr) (a : nat -> S) (v : nat -> S),
+        let env : nat -> D := fun j => (a j, v j) in
         defined_at e a ->
-        real (eval_dual e (a, 1%S)) = eval e a /\
-        du (eval_dual e (a, 1%S))   = eval (deriv e) a.
+        real (eval_dual e env) = eval e a /\
+        du (eval_dual e env)   = eval (deriv e v) a.
 Proof.
-    induction e; intros a Hdef; cbn in Hdef.
+    induction e; cbv zeta; intros a v Hdef; cbn in Hdef.
     - (* Const c *)
       cbn. unfold inj. now split.
     - (* Var *)
       cbn. now split.
     - (* Add e1 e2 *)
       destruct Hdef as [H1 H2].
-      destruct (IHe1 a H1) as [Hr1 Hd1].
-      destruct (IHe2 a H2) as [Hr2 Hd2].
+      destruct (IHe1 a v H1) as [Hr1 Hd1].
+      destruct (IHe2 a v H2) as [Hr2 Hd2].
       cbn. rewrite Hr1, Hr2, Hd1, Hd2. now split.
     - (* Sub e1 e2 *)
       destruct Hdef as [H1 H2].
-      destruct (IHe1 a H1) as [Hr1 Hd1].
-      destruct (IHe2 a H2) as [Hr2 Hd2].
+      destruct (IHe1 a v H1) as [Hr1 Hd1].
+      destruct (IHe2 a v H2) as [Hr2 Hd2].
       cbn. unfold Dual.sub.
       rewrite Hr1, Hr2, Hd1, Hd2.
       split; ring.
     - (* Mul e1 e2 *)
       destruct Hdef as [H1 H2].
-      destruct (IHe1 a H1) as [Hr1 Hd1].
-      destruct (IHe2 a H2) as [Hr2 Hd2].
+      destruct (IHe1 a v H1) as [Hr1 Hd1].
+      destruct (IHe2 a v H2) as [Hr2 Hd2].
       cbn. rewrite Hr1, Hr2, Hd1, Hd2.
       split; ring.
     - (* Opp e1 *)
-      destruct (IHe a Hdef) as [Hr Hd].
+      destruct (IHe a v Hdef) as [Hr Hd].
       cbn. rewrite Hr, Hd. split; ring.
     - (* Div e1 e2 *)
       destruct Hdef as [H1 [H2 Hnz]].
-      destruct (IHe1 a H1) as [Hr1 Hd1].
-      destruct (IHe2 a H2) as [Hr2 Hd2].
+      destruct (IHe1 a v H1) as [Hr1 Hd1].
+      destruct (IHe2 a v H2) as [Hr2 Hd2].
       cbn. split.
       + rewrite Hr1, Hr2.
         symmetry. apply div_def.
